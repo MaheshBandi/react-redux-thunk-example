bd79f5c8dd645b3f833b7602720b89a7c80f7114
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-25 00:45:36 +0530

    refactor code to handle memoization issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,35 @@
 import jsonPlaceHolder from  '../apis/jsonPlaceHolder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = ()=>async (dispatch,getState) =>{
+   await dispatch(fetchPosts()); //await is ony used here if we want to previous api response and process next api
+   const userIds = _.uniq(_.map(getState().posts,'userId'));
+   userIds.forEach(id=>dispatch(fetchUser(id)));
+
+   //alternative syntax
+  /* _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id=>dispatch(fetchUser(id)))
+    .value();
+    */
+    
+}
 
 // use redux thunk and define a function that return another function
-export const fetchPost = ()=> async dispatch=>{
+ const fetchPosts = ()=> async dispatch=>{
     const response = await jsonPlaceHolder.get('posts');
     dispatch({
         type:'FETCH_POSTS',
         payload:response.data
     })
 };
+ const fetchUser = (id)=>async dispatch=>{
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({
+        type:'FETCH_USER',
+        payload:response.data
+    });
+
+};
+
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions';
+import {fetchPostsAndUsers} from '../actions';
+import UserHeader from  '../components/UserHeader';
 
 class PostList extends React.Component{
     componentDidMount(){
-        this.props.fetchPost();
+        this.props.fetchPostsAndUsers();
     }
     renderList(){
         return this.props.posts.map(post=>{
@@ -14,8 +15,9 @@ class PostList extends React.Component{
                     <div className="content">
                         <div className="description">
                             <h2>{post.title}</h2>
-                            <p>{post.body}</p>
+                            <p>{post.body}</p>                            
                         </div>
+                        <UserHeader userId={post.userId}/>
                     </div>
                 </div>
             )
@@ -30,4 +32,4 @@ class PostList extends React.Component{
 const mapStateToProps = (state)=>{
     return {posts:state.posts};
 }
-export default connect(mapStateToProps,{fetchPost})(PostList);
+export default connect(mapStateToProps,{fetchPostsAndUsers})(PostList);
